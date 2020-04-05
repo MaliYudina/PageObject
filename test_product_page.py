b9eadecc917e68f9ebefd6837637ef7bc092fe39
Mali <maliyudina@gmail.com>
2020-04-05 19:11:07 +0300

    added class for tests

--- test_product_page.py
+++ test_product_page.py
@@ -10,56 +10,54 @@ import pytest
 import time
 
 
-@pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    """
-    Guest can add items to the basket, the name and price of the product
-    shall be equal to the name and price in the basket after submit button click
-    """
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    page = ProductPage(browser, link)
-    page.open()
-    product_page = ProductPage(browser, browser.current_url)
-    product_page.add_to_basket()
-    product_page.solve_quiz_and_get_code()
-    product_page.check_if_item_added_to_basket()
+class TestGuestUserAddToBasketFromProductPage:
+    @pytest.mark.need_review
+    def test_guest_can_add_product_to_basket(self, browser):
+        """
+        Guest can add items to the basket, the name and price of the product
+        shall be equal to the name and price in the basket after submit button click
+        """
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
+        page.open()
+        product_page = ProductPage(browser, browser.current_url)
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.check_if_item_added_to_basket()
 
+    @pytest.mark.need_review
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        """
+        Guest redirected to the basket page without submitting the basket button.
+        The basket shall be empty and 'empty message' is present.
+        """
+        link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-girl-who-played-with-non-fire_203/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_empty_basket()
 
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    """
-    Guest redirected to the basket page without submitting the basket button.
-    The basket shall be empty and 'empty message' is present.
-    """
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-girl-who-played-with-non-fire_203/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_empty_basket()
+    @pytest.mark.need_review
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        """
+        Guest can open login page from any product page
+        """
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/google-hacking_197/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    """
-    Guest can open login page from any product page
-    """
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/google-hacking_197/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    """
-    Guest can see login link with 'login' text value from any product page
-    """
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        """
+        Guest can see login link with 'login' text value from any product page
+        """
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 class TestUserAddToBasketFromProductPage:
